713b8a2256c18bb8200b2950cc0336586fa64d8f
bengris32 <bengris32@protonmail.ch>
2023-10-26 09:16:36 +0530

    lisaa: Enable support for F2FS formatted userdata
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I34a6daaec6e727c9caa0abe36449b93b4d3dd936

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,6 +63,7 @@ BOARD_HAS_MTK_HARDWARE := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt6895
+TARGET_USERIMAGES_USE_F2FS := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
