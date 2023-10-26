a880138489e499f872f83903a745a46969a0d857
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-10-26 10:03:01 +0530

    lisaa: Build default health HIDL
    Change-Id: I72bf9b21274d27952997b9ddcb3f1ae1a7ad8013

--- device.mk
+++ device.mk
@@ -61,6 +61,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     factory_init.connectivity.common.rc \
